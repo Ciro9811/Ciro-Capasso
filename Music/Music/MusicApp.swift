510d542efc6d7978c5e7ed20d11efaafac2da85c
Ciro <ccapasso22@fed.idserve.net>
2022-11-16 15:22:15 +0100

    Creation project

---
+++ Music/Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Ciro Capasso on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
